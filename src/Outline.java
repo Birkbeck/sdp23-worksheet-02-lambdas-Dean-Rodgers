55f0b368086dcea4a83e4e61b1c8cae782755c91
Dean <dean.rodgers5@gmail.com>
2023-04-04 21:02:47 +0100

    first commit

--- src/Outline.java
+++ src/Outline.java
@@ -1,9 +1,23 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class Outline {
+public class Outline  {
+
   public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1,7,3,4,8,2};
-    System.out.println(Arrays.asList(intArray));
-    // Arrays.sort(intArray,.......)
-  } 
+    String[] strArray = {"foo","bar","hello","jim","how","are","you"};
+    System.out.println(Arrays.asList(strArray));
+
+    // sort by length (shortest to longest)
+    Arrays.sort(strArray, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return o1.compareTo(o2);
+      }
+    });
+
+  }
+
+
 }
+
+
