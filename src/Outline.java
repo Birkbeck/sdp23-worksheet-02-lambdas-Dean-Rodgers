a698a0498eb92152d4bcf568666f73eff1afccfa
Dean <dean.rodgers5@gmail.com>
2023-04-04 23:34:15 +0100

    finished question 1

--- src/Outline.java
+++ src/Outline.java
@@ -3,17 +3,77 @@ import java.util.Comparator;
 
 public class Outline  {
 
-  public static void main(String... args) { // varargs alternative to String[]
-    String[] strArray = {"foo","bar","hello","jim","how","are","you"};
-    System.out.println(Arrays.asList(strArray));
+  public static String[] getWords() {
+    return new String[] {"foo","bar","hello","hi","jim","how","are","you"};
+  }
 
-    // sort by length (shortest to longest)
+  public static void question1_1_without_lambda() {
+    String[] strArray = getWords();
     Arrays.sort(strArray, new Comparator<String>() {
       @Override
       public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        if (o1.length() > o2.length()) {return 1;}
+        if (o1.length() < o2.length()) {return -1;}
+        return 0;
       }
     });
+    System.out.println("1.1 without lambda: " + Arrays.asList(strArray));
+  }
+
+
+  // length i.e. shortest to longest
+  public static void question1_1() {
+    String[] strArray = getWords();
+    Arrays.sort(strArray, (a, b) -> Integer.compare(a.length(), b.length()));
+    System.out.println("1.1: " + Arrays.asList(strArray));
+  }
+
+
+  // length i.e. shortest to longest
+  public static void question1_2() {
+    String[] strArray = getWords();
+    Arrays.sort(strArray, (a, b) -> Integer.compare(b.length(), a.length()));
+    System.out.println("1.2: " + Arrays.asList(strArray));
+  }
+
+
+  public static void question1_3() {
+    String[] strArray = getWords();
+    Arrays.sort(strArray, (a, b) -> Character.compare(a.charAt(0), b.charAt(0)));
+    System.out.println("1.3: " + Arrays.asList(strArray));
+  }
+
+
+  public static void question1_4() {
+    String[] strArray = getWords();
+    Arrays.sort(strArray, (a, b) -> {
+      if (a.contains("e") && !b.contains("e")) return -1;
+      if (b.contains("e") && !a.contains("e")) return 1;
+      return 0;
+    });
+    System.out.println("1.4: " + Arrays.asList(strArray));
+  }
+
+  public static void question2() {
+    String[] strArray = getWords();
+    Arrays.sort(strArray, StringUtils::eChecker);
+    System.out.println("2: " + Arrays.asList(strArray));
+
+  }
+
+
+
+  public static void main(String... args) { // varargs alternative to String[]
+
+    question1_1_without_lambda();
+    question1_1();
+    question1_2();
+    question1_3();
+    question1_4();
+    question2();
+
+
+
 
   }
 
---
+++ src/StringUtils.java
@@ -0,0 +1,10 @@
+public class StringUtils {
+
+    public static int eChecker(String s1, String s2) {
+        if (s1.contains("e") && !s2.contains("e")) return -1;
+        if (s1.contains("e") && !s2.contains("e")) return 1;
+        return 0;
+
+    }
+
+}
